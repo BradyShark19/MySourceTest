8fb719c222a54260eac2e4776c4dccdf95475215
raulBrady <bradyraule@gmail.com>
2021-01-18 08:47:00 -0500

    Sincronizando Proyecto

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Document</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="Librerias/bootstrap/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="Librerias/alertifyjs/css/alertify.css"    
+
+        <script src="Librerias/jquery-3.5.1.min.js"></script>
+        <script src="Librerias/bootstrap/js/bootstrap.js"></script>
+    </head>
+    
+    <body>
+    
+    </body>
+</html>
